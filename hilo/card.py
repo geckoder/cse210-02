a7f68413343ddadc9c6c21b7e75f004a186c7f70
Emily <cas20037@byui.edu>
2022-01-28 14:44:52 -0700

    comments

--- hilo/card.py
+++ hilo/card.py
@@ -2,18 +2,18 @@ import random
 
 
 class Card:
-    '''Attributes: card_num(int)'''
+    '''A card that show a random number on it.
+    Attributes: random_card(int)'''
 
     def __init__(self):
         '''Constructs a new instance of Card.
         Arg: self'''
         # create a card object. This can be used for the current_card and next_card
         self.random_card = 0
-        # this makes it an integer
 
     def draw(self):
         '''Draws a random card.
         Arg: self'''
         # The action of 'drawing' a new card
-        # assign the card a random number of 1-13
+        # assign the card a random number between 1-13
         self.random_card = random.randint(1, 13)
